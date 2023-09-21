d0b2dfb8b2d395a8947ed410c94b00f624eac2fc
alexcarvajal <alexcarvajal@unicauca.edu.co>
2023-09-20 20:46:11 -0500

    change component Bubble

--- src/components/Flowers/FlowerPurple.jsx
+++ src/components/Flowers/FlowerPurple.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { SlArrowLeft } from "react-icons/sl";
 import '../../styles/bubble.css'
-function FlowerPurple (){
+import Bubble from "./Bubble";
+function FlowerPurple() {
   const rows = 5;
   const cols = 5;
   const totalBubbles = rows * cols;
@@ -18,43 +19,9 @@ function FlowerPurple (){
     setIsImageOpaque(!allBubblesPopped);
   };
   return (
-
-<div 
-    style={{
-      display: 'flex',
-      flexDirection:'column',
-      justifyContent: 'center',
-      backgroundColor: 'rgba(198,115,255,0.2)',
-      alignItems: 'center',
-      height: '100vh',  // Esto asegurará que tome todo el alto de la vista del navegador
-    }}> 
-      <button className="backbutton">
-        <Link to='/yellow'>
-          <SlArrowLeft /> 
-        </Link>
-      </button>
-      <h1 style={{ margin: '20px 0 20px 0' }}>Splish Splash!</h1>
-    <div
-    className="container"
-    style={{
-      display: "grid",
-      gridTemplateRows: `repeat(${rows}, 1fr)`,
-      gridTemplateColumns: `repeat(${cols}, 1fr)`,
-      opacity: isImageOpaque ? 0.5 : 1, 
-    }}
-  >
-    {bubbles.map((isBubbleVisible, index) => (
-        <div
-          key={index}
-          className={isBubbleVisible ? "bubble" : "bubble-popped"}
-          onClick={() => handleBubbleClick(index)}
-        ></div>
-      ))}
-    </div>
+    <div>
+      <Bubble />
     </div>
   )
 }
 export default FlowerPurple
-
-
-
